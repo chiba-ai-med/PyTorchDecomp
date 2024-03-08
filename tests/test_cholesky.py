29c9d56a7f1135f4f9c7767988dc84a47b0e1f37
kokitsuyuzaki <k.t.the-answer@hotmail.co.jp>
2024-03-08 17:23:08 +0900

    feat: Sphinx Docs

--- tests/test_cholesky.py
+++ tests/test_cholesky.py
@@ -5,8 +5,7 @@ import numpy as np
 
 
 def test_CholeskyLayer():
-    x = torch.randn(6, 6)
-    x = torch.mm(x, x.t())
+    x = torch.tensor([[1, 2, 3], [2, 4, 5], [3, 5, 6]])
     cholesky_layer = td.CholeskyLayer(x)
     assert cholesky_layer.L.size()[0] == 6
     assert cholesky_layer.L.size()[1] == 6
